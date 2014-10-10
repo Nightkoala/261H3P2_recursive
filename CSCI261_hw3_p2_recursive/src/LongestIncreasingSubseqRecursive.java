7a46271631f5a637d7b71e023b8f187d5ec7be5c
Derek Brown <djb3718@rit.edu>
2014-10-09 20:10:50 -0400

    Created the java file for the algorithm

---
+++ CSCI261_hw3_p2_recursive/src/LongestIncreasingSubseqRecursive.java
@@ -0,0 +1,15 @@
+/*
+ * LongestIncreasingSubseqRecursive.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ * 
+ * Purpose	A recursive implementation for finding the longest subsequence of
+ * 			increasing numbers in an array
+ */
+
+public class LongestIncreasingSubseqRecursive {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end LongestIncreasingSubseqRecursive
